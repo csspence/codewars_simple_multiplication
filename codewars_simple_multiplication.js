7ef8186e9b9a7dc499311184692938f3f592ba9e
Crew Spence <csspence123@gmail.com>
2020-12-05 07:57:58 -0600

    Wrote working solution.

--- codewars_simple_multiplication.js
+++ codewars_simple_multiplication.js
@@ -2,6 +2,7 @@
 This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 */
 
-function simpleMultiplication(number) {
-    // your code........
+const simpleMultiplication = (number) => {
+    let verdict = number % 2 === 0;
+    return verdict === true ? number * 8 : number * 9;
 }
